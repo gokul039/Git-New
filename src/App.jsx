2aafc25653bc08f8bf69da1db526d3e7705498c9
gokul039 <rgokulramesh12@gmail.com>
2025-09-09 10:34:25 +0530

    initial commmit

--- src/App.jsx
+++ src/App.jsx
@@ -12,11 +12,11 @@ function App() {
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
+        <a href="https://react.dev" target="_blank">git
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>this is branch3</h1>
+      <h1>this is branch1</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
